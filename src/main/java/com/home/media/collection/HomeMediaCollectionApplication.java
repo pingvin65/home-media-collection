05b11f9d3e89caa72683d1a01eca7e1e231e4ff6
Sasa <budai8369@gmail.com>
2020-07-13 17:35:24 -0400

    first commit

---
+++ src/main/java/com/home/media/collection/HomeMediaCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.home.media.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeMediaCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeMediaCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/media/collection/HomeMediaCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.media.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeMediaCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
